6ffe855c6443071fc2003712f0e41cd57ef445e9
Maxim Serdyukov <hempjuice@gmail.com>
2020-12-12 12:57:50 +0500

    Исправление ошибок

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from time import time
 from functools import wraps
 
 
-# Функция возводит переданный список числел в степень degree и возвращает результат
+EVEN_NUMBERS = 0
+ODD_NUMBERS = 1
+PRIME_NUMBERS = 2
+
+
 def exponentiation(numbers, degree=2):
+    """ Функция возводит переданный список числел в степень degree и возвращает результат """
     result = []
 
     for n in numbers:
@@ -12,8 +17,8 @@ def exponentiation(numbers, degree=2):
     return result
 
 
-# Функция возвращает True, если переданное число простое
 def is_prime(number):
+    """ Функция возвращает True, если переданное число простое """
     count = 0
 
     for i in range(1, number + 1):
@@ -21,11 +26,6 @@ def is_prime(number):
             count += 1
 
     return count == 2
-
-
-EVEN_NUMBERS = 0
-ODD_NUMBERS = 1
-PRIME_NUMBERS = 2
 
 
 def time_call(func):
@@ -41,9 +41,9 @@ def time_call(func):
     return wrapper
 
 
-# Функция отфильтровывает переданный список чисел по заданному отбору (четные, нечетные, простые)
 @time_call
 def filter_numbers(numbers, flt):
+    """ Функция отфильтровывает переданный список чисел по заданному отбору (четные, нечетные, простые) """
     if flt == EVEN_NUMBERS:
         func = lambda n: n % 2 == 0
     elif flt == ODD_NUMBERS:
@@ -54,8 +54,11 @@ def filter_numbers(numbers, flt):
     return list(filter(func, numbers))
 
 
-#Функция вычисляет n число Фибоначчи
 def fibonacci(n):
+    """ Функция вычисляет n число Фибоначчи """
+    if n <= 0:
+        return None
+
     result = 1
 
     if n > 2:
@@ -64,11 +67,12 @@ def fibonacci(n):
     return result
 
 
-numbers = [1, 2, 3, 4, 5]
-print('Квадраты чисел', numbers, ':', exponentiation(numbers))
-print('Только четные числа из списка', numbers, ':', filter_numbers(numbers, EVEN_NUMBERS))
-print('Только нечетные числа из списка', numbers, ':', filter_numbers(numbers, ODD_NUMBERS))
-print('Только простые числа из списка', numbers, ':', filter_numbers(numbers, PRIME_NUMBERS))
+if __name__ == "__main__":
+    numbers = [1, 2, 3, 4, 5]
+    print('Квадраты чисел', numbers, ':', exponentiation(numbers))
+    print('Только четные числа из списка', numbers, ':', filter_numbers(numbers, EVEN_NUMBERS))
+    print('Только нечетные числа из списка', numbers, ':', filter_numbers(numbers, ODD_NUMBERS))
+    print('Только простые числа из списка', numbers, ':', filter_numbers(numbers, PRIME_NUMBERS))
 
-fib_number = 10
-print(fib_number, 'число Фибоначчи:', fibonacci(fib_number))
+    fib_number = 10
+    print(fib_number, 'число Фибоначчи:', fibonacci(fib_number))
